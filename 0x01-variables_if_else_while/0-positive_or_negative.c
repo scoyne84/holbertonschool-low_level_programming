edc410d94ce5ce827843d8c308721e323fb26e48
scoyne84 <scoyne84@yahoo.com>
2017-05-11 20:31:04 +0000

    removed {} from some lines

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-{
+
 if (n < 0)
 printf("is negative");
-}
-{
+
+
 if (n > 0)
 printf("is positive");
-}
-{
+
+
 if (n = 0)
 printf("is zero");
-}
+
 return (0);
 }
