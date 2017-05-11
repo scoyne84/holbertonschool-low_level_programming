1222e3173da6fcac1b01f81133539f99707e9f21
scoyne84 <scoyne84@yahoo.com>
2017-05-11 01:11:14 +0000

    no spaces for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-  printf("Size of a char: %d bytes(s)\n", sizeof(char));
-  printf("Size of an int: %d bytes(s)\n", sizeof(char));
-  printf("Size of a long int: %d bytes(s)\n", sizeof(char));
-  printf("Size of a long long int: %d bytes(s)\n", sizeof(char));
-  printf("Size of a float: %d bytes(s)\n", sizeof(char));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of an int: %d bytes(s)\n", sizeof(char));
+printf("Size of a long int: %d bytes(s)\n", sizeof(char));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(char));
+printf("Size of a float: %d bytes(s)\n", sizeof(char));
   return (0);
 }
