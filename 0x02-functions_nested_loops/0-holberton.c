4e8544bd176f3e19a36f2733de235e09cd990308
scoyne84 <scoyne84@yahoo.com>
2017-05-16 17:19:59 +0000

    add task 0 for testing

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i = 0;
+	char s[]="Holberton\n";
+
+	while (s[i])
+	{
+		_putchar (s[i]);
+		i++;
+	}
+	return(0);
+}
