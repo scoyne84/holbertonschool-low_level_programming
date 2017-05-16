17356b30418f99fb285068247388cc537d435006
scoyne84 <scoyne84@yahoo.com>
2017-05-16 19:22:29 +0000

    edit notes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - check the code for Holberton School students.
  *
- * Return: Always 0.
+ * Return: void.
  */
 void print_alphabet_x10(void)
 {
