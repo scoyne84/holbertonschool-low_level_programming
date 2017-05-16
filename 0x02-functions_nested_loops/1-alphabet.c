d002af15ca69cebf1c51b092bc9614adcd1f7747
scoyne84 <scoyne84@yahoo.com>
2017-05-16 18:57:39 +0000

    remove extra lines and update notes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * void - check the code for Holberton School students.
  *
- * Return: Always 0.
+ * Return: void
  */
 void print_alphabet(void)
 {
@@ -14,5 +14,4 @@ void print_alphabet(void)
 		_putchar (az);
 	}
 	_putchar ('\n');
-
 }
