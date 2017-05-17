8519df0568f2b90ef0d5016a851fe23d2446cc90
scoyne84 <scoyne84@yahoo.com>
2017-05-17 22:27:04 +0000

    add task 8 for testing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,31 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - check the code for Holberton School students.
+ * jack_bauer - prints every minute of the day from 00:00 to 23:59.
  *
- * Return: Always 0.
+ * Return: void.
  */
 void jack_bauer(void)
 {
-	int w, x, y, z
-  	
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		int c = a / 10;
+		int d = a % 10;
+
+		for (b = 0; b <= 59; b++)
+		{
+			int x = b / 10;
+			int y = b % 10;
+
+			_putchar((c) + '0');
+			_putchar((d) + '0');
+			_putchar(':');
+			_putchar((x) + '0');
+			_putchar((y) + '0');
+			_putchar('\n');
+		}
+	}
 }
