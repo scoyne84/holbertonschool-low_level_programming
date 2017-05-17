8d3281e80bf5d3ebc7466632cdf4f9f56966699b
scoyne84 <scoyne84@yahoo.com>
2017-05-17 21:33:21 +0000

    add task 7 for testing

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,24 @@
 /**
  * main - check the code for Holberton School students.
  * @x: integer being tested
- * Return: Always 0.
+ * Return: lastn
  */
 int print_last_digit(int x)
 {
+	char lastn;
+
+	if (x >= 0)
+	{
+		lastn = (x % 10);
+		_putchar(lastn + '0');
+
+		return (lastn);
+	}
+	else
+	{
+		lastn = (x % 10) * -1;
+		_putchar(lastn + '0');
+
+		return (lastn);
+	}
+}
