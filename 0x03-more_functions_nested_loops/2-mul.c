4438a979a5144da32263f33c6d484eb2970cd29c
scoyne84 <scoyne84@yahoo.com>
2017-05-18 20:09:32 +0000

    add task 2 for testing

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies variables.
+ * @a: variable a
+ * @b: variable b
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
