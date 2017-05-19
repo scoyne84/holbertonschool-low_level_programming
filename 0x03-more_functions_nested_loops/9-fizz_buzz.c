329e413bd4d766ac1ca19199f2b4ab1fec4021df
scoyne84 <scoyne84@yahoo.com>
2017-05-19 23:32:16 +0000

    add task 9 for initial testing

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 99; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	if (n == 100)
+	{
+		printf("Buzz\n");
+	}
+	return (0);
+}
