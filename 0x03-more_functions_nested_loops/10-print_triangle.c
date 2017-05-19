44fe9361cea8f0301b8f82f3555ef4c7cba607f1
scoyne84 <scoyne84@yahoo.com>
2017-05-19 19:41:00 +0000

    add task 10 for testing

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - check the code for Holberton School students.
+ * @size: represents size of the triangle
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+	int spaces;
+	int amp;
+	int i;
+	int j;
+	int k;
+
+	if (size == '0')
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++)
+	{
+		spaces = (size - i);
+		amp = (size - spaces);
+
+		for (j = 1; j <= spaces; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= amp; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
