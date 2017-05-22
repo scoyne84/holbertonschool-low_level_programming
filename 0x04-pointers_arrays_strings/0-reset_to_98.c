6ad63a7ff10943bb4cdaf76a0fc0da6b3a1cd5fb
scoyne84 <scoyne84@yahoo.com>
2017-05-22 22:48:29 +0000

    add task 0 for testing

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - set the integer to 98.
+ * @n: pointer to the integer we want to set to 98
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
