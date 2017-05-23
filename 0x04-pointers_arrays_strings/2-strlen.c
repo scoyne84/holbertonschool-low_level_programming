d72fe70244e4dbda52664a359917a4ea616cc11a
scoyne84 <scoyne84@yahoo.com>
2017-05-23 19:46:08 +0000

    initial upload of task 3 for testing

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @s: variable representing string
+ * Return: void.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 
