a4d687d946d1388f40626b66ae3d978d06a724bd
scoyne84 <scoyne84@yahoo.com>
2017-05-23 22:03:13 +0000

    add task 6

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other element.
+ * @str: string to be input
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; x <= 9/2; x++)
+	{
+		
