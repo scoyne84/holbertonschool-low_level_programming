9cda0fb51c8165dda4177160c8e52d6b002933af
scoyne84 <scoyne84@yahoo.com>
2017-05-24 20:44:16 +0000

    add task 8 for initial testing

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- * @a: 
- * @n: number of elements to print
- * Return: Always 0.
- */
+* print_array - prints elements in array.
+* @a: pointer
+* @n: number of elements to print
+* Return: Always 0.
+*/
+
 void print_array(int *a, int n)
 {
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[x]);
+	}
+	printf("\n");
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -7,3 +7,4 @@ void print_rev(char *s);
 void puts2(char *str);
 void rev_string(char *s);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
