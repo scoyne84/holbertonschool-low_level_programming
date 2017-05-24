2dad0c7a71b09135fcf11039ee0d74e387f009a6
scoyne84 <scoyne84@yahoo.com>
2017-05-24 22:55:28 +0000

    correction to if loop

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -12,22 +12,21 @@ void puts_half(char *str)
 
 	while (str[x])
 	{
-		x++;
-		if (x % 2 == 0)
-		{
-			y = (x / 2);
-		}
-		else
-		{
-			y = (x / 2 + 1);
-		}
+	x++;
+	if (x % 2 == 0)
+	{
+		y = (x / 2);
+	}
+	else
+	{
+		y = (x / 2 + 1);
+	}
 	}
 	while (str[y])
 	{
 		_putchar(str[y]);
 		y++;
 	}
-	_putchar(str[y]);
 	_putchar('\n');
 }
 
