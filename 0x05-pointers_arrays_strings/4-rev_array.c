406624e13d064b849a215b7a27dc53ab9e29f0be
scoyne84 <scoyne84@yahoo.com>
2017-05-26 21:22:46 +0000

    correct while loop

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -4,20 +4,20 @@
  * reverse_array - reverses the given array.
  * @a: an array of integers
  * @n: the number of elements to swap
- *
- * Return: nothing.
+ * Return: void.
  */
 void reverse_array(int *a, int n)
 {
 	int t;
 	int i = 0;
-	int j = n - 1;
+	int j = (n - 1);
 
-	while (i - j)
+	while (i < n/2)
 	{
 		t = a[i];
 		a[i] = a[j];
 		a[j] = t;
+
 		i++;
 		j--;
 	}
