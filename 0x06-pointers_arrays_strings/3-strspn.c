624f27d00e7b133022200ea730deb7b757c5d83a
scoyne84 <scoyne84@yahoo.com>
2017-05-31 20:36:21 +0000

    removed break from outter loop

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -8,21 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int n = 0;
 	int i, j;
 
 	for (i = 0; s[i]; i++)
 	{
-		if (n != i)
-			break;
 		for (j = 0; accept[j]; j++)
 		{
-			break;
-			if (s[i] == accept[j])
+			if (accept[j] == s[i])
 			{
-				n++;
+				break;
 			}
 		}
+		if (!accept[j])
+		{
+			break;
+		}
 	}
-	return (j);
+	return (i);
 }
