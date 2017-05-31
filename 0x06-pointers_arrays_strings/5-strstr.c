547d748c30dcbca6e65fe5c63e6d1091ea4a74ea
scoyne84 <scoyne84@yahoo.com>
2017-05-31 18:08:43 +0000

    add task 5 for testing

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: string containing substring.
+ * @needle: substring to locate
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (0);
+}
