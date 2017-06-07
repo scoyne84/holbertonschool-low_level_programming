f33ec4668bf3e6b8132418a8f05c96bd21440704
scoyne84 <scoyne84@yahoo.com>
2017-06-07 20:51:42 +0000

    add description

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-
+/**
+ * main - prints program name 
+ * @argc: arguments passed
+ * @argv: array that holds string values
+ */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
