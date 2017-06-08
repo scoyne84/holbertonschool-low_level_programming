63b8b5888e582d8545e91b05b6dd02d04baa9490
scoyne84 <scoyne84@yahoo.com>
2017-06-08 01:22:13 +0000

    corrected loop error

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
+#include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - adds positive numbers
  * @argc: arguments passed to program
@@ -8,12 +9,21 @@
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int x, y, result;
+	int x, y, z;
+	int result;
 
 	for (x = 1; x < argc; x++)
 	{
-		y = atoi(argv[x]);
-		result += y;
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] < '0' || argv[x][y] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		z = atoi(argv[x]);
+		result += z;
 	}
 	printf("%d\n", result);
 	return (0);
