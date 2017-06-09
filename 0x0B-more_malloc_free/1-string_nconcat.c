4c9ba0fa5f41c5e7cf32ff695b9961e5624c364a
scoyne84 <scoyne84@yahoo.com>
2017-06-09 19:22:47 +0000

    checked header files

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
  * @n: position in array
  * Return: Always 0.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 	unsigned int i, j;
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 		p[i] = s1[i];
 	}
 	j = i;
-	for (i = 0; s2 && (s2[i] != '\0') && (i < n); i++)
+	for (i = 0; s2 && (s2[i] != '\0' && i < n); i++)
 	{
 		p[j++] = s2[i];
 	}
