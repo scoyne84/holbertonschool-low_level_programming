84a9214ba7c82715a64d3dd33f7d73f886a3b6ea
scoyne84 <scoyne84@yahoo.com>
2017-06-09 22:55:49 +0000

    removed whitespace

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -15,11 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 	for (a = 0; s1[a]; a++)
 	{
