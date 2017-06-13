606efcb0ea5af10e51843d7795758d780240289d
scoyne84 <scoyne84@yahoo.com>
2017-06-13 23:08:42 +0000

    corrected dog.h header file

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -14,7 +14,9 @@ typedef struct dog
 } my_dog;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-typedef struct dog
+
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
