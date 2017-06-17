605225aaf5412fd43e74996451eac0088857f730
scoyne84 <scoyne84@yahoo.com>
2017-06-17 20:25:03 +0000

    add task 2 for initial testing

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,2 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ * Return: void.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list list;
+	unsigned int x;
+	char *y;
+
+	va_start(list, n);
+	{
+		for (x = 0; x < n; x++)
+		{
+			y = va_arg(list, char *);
+			if (y != NULL)
+			{
+				printf("%s", y);
+			}
+			else if (y == NULL)
+			{
+				printf("(nil)");
+			}
+			if (separator != NULL && x < n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
