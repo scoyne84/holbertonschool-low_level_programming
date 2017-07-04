3f8c0402c25712452165d443ffe480a318399d91
Steven Coyne <stevencoyne@steven-coynes-computer.local>
2017-07-03 22:22:04 -0700

    add main files for tasks 0-3 and task 0 solution for initial testing.

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads text file and prints to POSIX std output
+ * @filename: filename
+ * @letters: number of letters to be read and printed
+ * Return: the amount of letters it is capable of printing
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  int a;
+  ssize_t x;
+  char *buffer = malloc(sizeof(char) * letters);
+
+  if (filename == NULL)
+    return (0);
+  a = open(filename, O_RDONLY);
+  if (a == -1)
+    return (0);
+  if (buffer == NULL)
+    {
+      close(a);
+      return (0);
+    }
+  x = read(a, buffer, letters);
+  close(a);
+  if (x == -1)
+    {
+      free(buffer);
+      return (0);
+    }
+  write(STDOUT_FILENO, buffer, x);
+  free(buffer);
+  return (x);
+} 
