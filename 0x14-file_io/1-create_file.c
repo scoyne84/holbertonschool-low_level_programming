8c8aed717d41cb245b6d242fdd0c5543c94a34ee
scoyne84 <scoyne84@yahoo.com>
2017-07-06 19:57:29 +0000

    removed quotation marks from filename

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -4,30 +4,30 @@
 
 /**
  * create_file - creates a file.
- * @filename:
- * @text_content:
+ * @filename: name of file to create
+ * @text_content: content of file
  * Return: 1 on success, -1 on failure or NULL.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t file = 0;
-	size_t len;
+	size_t length;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open("filename", O_RDONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	for (len = 0; text_content && text_content[len]; len++)
+	for (length = 0; text_content && text_content[length]; length++)
 		;
-	if (text_content && len)
+	if (text_content && length)
 	{
-		file = write(fd, text_content, len);
+		file = write(fd, text_content, length);
 	}
 	close(fd);
 		if (file == -1)
