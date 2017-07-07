9fd802cdc5cb1985ccabb532978ce38de6ff46c3
scoyne84 <scoyne84@yahoo.com>
2017-07-07 19:11:01 +0000

    add task 3 for initial testing

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main -
+ * @argc:
+ * @argv:
+ * return:
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, ret1, ret2;
+	char buff[1204];
+
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		if_error1(fd1);
+	}
+	fd2 = open(argv[2], O_CREATE);
+	if (fd2 == -1)
+	{
+		if_error2(fd2);
+	}
+	do{
+		r_count = read(fd1, buff, 1204);
+		w_count = write(fd2, buff, r_count);
+		if (r_count != w_count)
+		{
+			ret1 = close(fd1);
+			if (ret1 == -1)
+			{
+				if_error1(fd1);
+			}
+			ret2 = close(fd2);
+			if (ret2 == -1)
+			{
+				if_error2(fd2);
+			}
+			if_error3(argv[2]);
+		}
+	}while(r_count != 0 || r_count != -1)
+	close(fd1);
+	{
+		if_error4(fd1);
+	}
+	close(fd2);
+	{
+		if_error5(fd2);
+	}
+	return(0);
+}
